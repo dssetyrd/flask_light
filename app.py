6a7015dbbc34c0b4caa211b0576c2dcabd56a7d4
dssetyrd <63414819+dssetyrd@users.noreply.github.com>
2020-04-10 14:27:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2 as cv
+#import cv2 as cv
 
 
-MAX_FILE_SIZE = 1024 * 1024*10 + 1
+MAX_FILE_SIZE = 1024 * 1024*5 + 1
 
 app = Flask(__name__)
 
@@ -15,10 +15,10 @@ def index():
     if request.method == "POST":
         file = request.files["file"]
         filestr = file.read()
-        npimg = np.fromstring(filestr, np.uint8)
-        img = cv.imdecode(npimg, cv.IMREAD_UNCHANGED)
-        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        plt.imsave("./templates/new.jpg",img)
+        #npimg = np.fromstring(filestr, np.uint8)
+        #img = cv.imdecode(npimg, cv.IMREAD_UNCHANGED)
+        #img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        #plt.imsave("./templates/new.jpg",img)
         #cv2.imwrite ("new.jpg",cv2.cvtColor(z, cv2.COLOR_BGR2RGB))
         if bool(file.filename):
             file_bytes = file.read(MAX_FILE_SIZE)
